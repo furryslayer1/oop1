07fdfeb2ac5e7687f6136eb4dacea9cd375206e0
furryslayer1 <72922168+furryslayer1@users.noreply.github.com>
2022-02-06 14:22:57 +0600

    Initial commit

---
+++ src/com/company/oop6/Main.java
@@ -0,0 +1,26 @@
+package com.company.oop6;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class Main {
+
+    public static List<Integer> createNewList() {
+        ArrayList<Integer> integers = new ArrayList<>();
+        Random random = new Random(System.currentTimeMillis());
+        for(int i = 0; i < 15; i++) {
+            int number = -15 + random.nextInt(16 + 15);
+            integers.add(number);
+        }
+        return integers;
+    }
+
+
+    public static void main(String[] args){
+     List<Integer> list = createNewList();
+     System.out.println(list);
+     List<Integer> result = list.stream().sorted((o1, o2) -> o2).collect(Collectors.toList());
+     System.out.println(result);
+ }
+}
+
